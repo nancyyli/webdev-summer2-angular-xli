30e04867c7b0393979a7c792d5706bcf480a7935
nancyyli <lixiang@ccs.neu.edu>
2018-08-04 18:31:38 -0400

    added simple profile form

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,12 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { WhiteBoardComponent } from "./white-board/white-board.component";
 import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
+import { ProfileComponent } from './profile/profile.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: WhiteBoardComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent},
+  { path: 'profile', component: ProfileComponent},
   { path: '**', component: WhiteBoardComponent} // last
 ];
 export const routing = RouterModule.forRoot(appRoutes);
