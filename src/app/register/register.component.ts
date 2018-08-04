3802a0cd11fa6fb9afe6fdf608d2f048ad929e83
nancyyli <lixiang@ccs.neu.edu>
2018-08-03 23:32:53 -0400

    added register typescript

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,52 @@
+
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {UserServiceClient} from "../services/user.service.client";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private router: Router,
+              private service: UserServiceClient) { }
+
+  username;
+  password;
+  password2;
+  validPassword = true;
+  validUsername = true;
+  validatePassword(password, password2) {
+      if(password != password2) {
+        this.validPassword=false;
+        return false;
+      }
+      else {
+        this.validPassword=true;
+        return true;
+      }
+  }
+
+  register(username, password, password2) {
+    if (this.validatePassword(password, password2)) {
+        this.service.findUserByUsername(username).then((username) => {
+          if (username.length > 0) {
+            this.validUsername = false;
+          }
+          else {
+            this.service
+            .createUser(username, password)
+            .then(() =>
+              this.router.navigate(['profile']));
+            this.validUsername = true;
+          }
+        });
+      }
+    }
+
+  ngOnInit() {
+  }
+
+}
