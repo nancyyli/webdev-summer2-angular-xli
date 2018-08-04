78d2900c8032f0d6946f0e0c9d94d10e1d6a7264
nancyyli <lixiang@ccs.neu.edu>
2018-08-03 23:59:46 -0400

    added login logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import {Route, Router} from "@angular/router";
+import {UserServiceClient} from "../services/user.service.client";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username;
+  password;
+  successLogin = true;
+
+  constructor(private router: Router,
+              private service: UserServiceClient) { }
+
+
+  login(username, password) {
+    this.service
+      .login(username, password)
+      .then((loggedIn) => {
+        if (loggedIn.length) {
+            this.successLogin = true;
+            this.router.navigate(['profile']);
+        }
+        else {
+          this.successLogin = false;
+        }
+      });
+  }
 
   ngOnInit() {
+
   }
 
 }
