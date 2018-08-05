35e1def9ea5b070186390d1d34bf4616892b47a4
nancyyli <lixiang@ccs.neu.edu>
2018-08-05 16:35:07 -0400

    render widgets form

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+ import {WidgetServiceClient} from "../services/widget.service.client";
 
 @Component({
   selector: 'app-widgets',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./widgets.component.css']
 })
 export class WidgetsComponent implements OnInit {
+  constructor(private service: WidgetServiceClient,
+    private route: ActivatedRoute) {
+      this.route.params.subscribe(params => this.setContext(params));
+    }
 
-  constructor() { }
+    context;
+    widgets = [];
+    setContext(params) {
+    this.context = params;
+    this.loadWidgets(params.lessonId);
+    }
+    loadWidgets(lessonId) {
+      if (lessonId) {
+        this.service.findWidgetsForLesson(lessonId)
+        .then(widgets => this.widgets = widgets);
+      }
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
