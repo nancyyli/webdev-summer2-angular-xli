82fb27b78ad371776968cbdb435a3e078f2bb013
nancyyli <lixiang@ccs.neu.edu>
2018-08-07 23:20:04 -0400

    changed url end point to heroku app

--- src/app/services/widget.service.client.ts
+++ src/app/services/widget.service.client.ts
@@ -1,6 +1,6 @@
 export class WidgetServiceClient {
     findWidgetsForLesson(lessonId) {
-      return fetch('http://localhost:8080/api/lesson/' + lessonId + '/widget')
+      return fetch('https://webdev-summer2-xli.herokuapp.com/api/lesson/' + lessonId + '/widget')
         .then(response => response.json());
     }
   }
