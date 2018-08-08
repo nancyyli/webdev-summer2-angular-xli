8ab7ca9e114c1dc009e568521b7be197bf61a9b5
nancyyli <lixiang@ccs.neu.edu>
2018-08-07 22:28:41 -0400

    lists current courses for registered user on homepage

--- src/app/course-grid/course-grid.component.ts
+++ src/app/course-grid/course-grid.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseServiceClient} from '../services/course.service.client';
+import {SectionServiceClient} from "../services/section.service.client";
+import {UserServiceClient} from '../services/user.service.client';
 
 @Component({
   selector: 'app-course-grid',
@@ -8,12 +10,41 @@ import {CourseServiceClient} from '../services/course.service.client';
 })
 export class CourseGridComponent implements OnInit {
   courses = [];
-  constructor(private courseService: CourseServiceClient) { }
+  currentCourses = [];
+  sections = [];
+  constructor(private courseService: CourseServiceClient, private sectionService: SectionServiceClient, private userService: UserServiceClient) { }
 
   ngOnInit() {
     this.courseService
     .findAllCourses()
     .then(courses => this.courses = courses);
+    this.loadCourses();
+  }
+  enrolledCourses(sectionId) {
+    this.sectionService.findSectionById(sectionId).then((section) => {
+        this.courseService.findCourseById(section.courseId)
+        .then(course => {
+          this.currentCourses.push(course);
+        });
+    });
+  }
+
+  loadCourses() {
+    this.userService.currentUser().then((user) => {
+      var currentUser = user;
+      if (Object.keys(currentUser).length) {
+        this.sectionService
+        .findSectionsForStudent()
+        .then(sections => {
+          sections.forEach(((section) => {
+            if (section.section) {
+              this.sections.push(section);
+              this.enrolledCourses(section.section._id);
+            }
+          }))
+        });
+      }
+    })
   }
 
 }
